40865e2e92639ddfdeb36f07aa818179f1e5a21f
Michael Geyer <mgeyer@ycp.edu>
2024-06-19 14:07:23 -0400

    Worked on test code

--- tools/GyroTest.cpp
+++ tools/GyroTest.cpp
@@ -5,30 +5,30 @@
 Adafruit_MPU6050 mpu;
 
 void setup(void) {
-  Serial.begin(115200);
-  while (!Serial)
-    delay(10); // will pause Zero, Leonardo, etc until serial console opens
+  //Serial.begin(115200);
+  // while (!Serial)
+  //   delay(10); // will pause Zero, Leonardo, etc until serial console opens
 
-  Serial.println("Adafruit MPU6050 test!");
+  // Serial.println("Adafruit MPU6050 test!");
 
   // Try to initialize!
   if (!mpu.begin()) {
-    Serial.println("Failed to find MPU6050 chip");
+    //Serial.println("Failed to find MPU6050 chip");
     while (1) {
       delay(10);
     }
   }
-  Serial.println("MPU6050 Found!");
+  //Serial.println("MPU6050 Found!");
 
   //setupt motion detection
-  mpu.setHighPassFilter(MPU6050_HIGHPASS_0_63_HZ);
-  mpu.setMotionDetectionThreshold(1);
-  mpu.setMotionDetectionDuration(20);
-  mpu.setInterruptPinLatch(true);	// Keep it latched.  Will turn off when reinitialized.
-  mpu.setInterruptPinPolarity(true);
-  mpu.setMotionInterrupt(true);
+  // mpu.setHighPassFilter(MPU6050_HIGHPASS_0_63_HZ);
+  // mpu.setMotionDetectionThreshold(1);
+  // mpu.setMotionDetectionDuration(20);
+  // mpu.setInterruptPinLatch(true);	// Keep it latched.  Will turn off when reinitialized.
+  // mpu.setInterruptPinPolarity(true);
+  // mpu.setMotionInterrupt(true);
 
-  Serial.println("");
+  //Serial.println("");
   delay(100);
 }
 
